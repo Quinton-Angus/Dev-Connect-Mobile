72c4a855ace833617bb2f812c9a5470cdaba51d1
Quinton Angus <quinton@quintondev.com>
2026-08-22 17:06:51 +0100

    Polish session card styling

--- Dev-Connect-Mobile/styling/sessionCard.js
+++ Dev-Connect-Mobile/styling/sessionCard.js
@@ -1,46 +1,52 @@
-import { width } from "@expo/ui/jetpack-compose/modifiers";
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     cardWrapper: {
-        width: '80%'
+        width: '100%',
+        paddingHorizontal: 18,
+        marginBottom: 14
     },
 
     card: {
         width: '100%',
-        height: 125,
+        minHeight: 125,
         backgroundColor: '#101010',
-        padding: 20,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: '#2A2A2A',
+        padding: 18,
         justifyContent: 'flex-start',
-        alignItems: 'flex-start'
+        alignItems: 'flex-start',
+        overflow: 'hidden'
     },
 
     id: {
         color: '#FFFFFF',
         fontFamily: 'SemiBold',
-        fontSize: 15,
+        fontSize: 18,
         marginBottom: 10
     },
 
     detail: {
         fontFamily: 'Light',
-        fontSize: 8,
-        color: '#FFFFFF',
-        marginBottom: 5
+        fontSize: 10,
+        color: '#BDBDBD',
+        marginBottom: 12
     },
 
     statusBar: {
         width: '100%',
-        height: 20,
+        minHeight: 28,
+        borderRadius: 7,
         justifyContent: 'center',
         alignItems: 'flex-start',
-        paddingLeft: 20,
+        paddingHorizontal: 12,
         backgroundColor: '#FFFFFF'
     },
 
     status: {
         fontFamily: 'SemiBold',
-        fontSize: 8,
+        fontSize: 9,
         color: '#101010'
     }
 })
